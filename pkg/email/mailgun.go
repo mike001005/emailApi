59111be68a2f17b818c1eb832169bdf1b9e94864
mike001005 <m_norris30@yahoo.com>
2018-01-03 01:09:50 -0800

    vars

--- pkg/email/mailgun.go
+++ pkg/email/mailgun.go
@@ -4,12 +4,10 @@ import (
 	"os"
 
 	"gopkg.in/mailgun/mailgun-go.v1"
-	"fmt"
 )
 
 // SendSimpleMessage Sends an email using Mail gun api
 func SendSimpleMessage(from string, toEmail string) (string, string, error) {
-	fmt.Println(os.Getenv("MG_DOMAIN"))
 	mg := mailgun.NewMailgun(os.Getenv("MG_DOMAIN"), os.Getenv("MG_API_KEY"), os.Getenv("MG_PUBLIC_API_KEY"))
 	msg := mailgun.NewMessage(from, "Message subject", "This is the message body", toEmail)
 	response, id, err := mg.Send(msg)
