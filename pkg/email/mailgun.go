5344b8fd5ea0f963bbe5c3e72d3fe1c9c70df6db
mike001005 <m_norris30@yahoo.com>
2018-01-02 16:00:44 -0800

    firstCommit

--- pkg/email/mailgun.go
+++ pkg/email/mailgun.go
@@ -4,6 +4,12 @@ import (
 	"gopkg.in/mailgun/mailgun-go.v1"
 )
 
+// InterfaceMailGun mailGun interface
+type InterfaceMailGun interface {
+	SendSimpleMessage(from string, toEmail string) (string, string, error)
+}
+
+
 // SendSimpleMessage Sends an email using Mail gun api
 func SendSimpleMessage(from string, toEmail string) (string, string, error) {
 	mg := mailgun.NewMailgun(MgDomain, MgAPIKey, MgPublicAPIKey)
--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -3,19 +3,17 @@
 set -e
 echo "" > coverage.txt
 
-
+go get golang.org/x/tools/cmd/goimports
 go get -u github.com/golang/lint/golint
 go get github.com/GeertJohan/fgt
 go get golang.org/x/tools/cmd/goimports
+go get -v -u github.com/gorilla/mux
+go get -v -u github.com/sendgrid/sendgrid-go
+go get -v -u gopkg.in/mailgun/mailgun-go.v1
+
 
 fgt goimports -l ./pkg/*
 fgt golint ./pkg/...
-
-#fgt goimports -l ./cmd/*
-#fgt golint ./cmd/...
-
-#fgt goimports -l ./internal/*
-#fgt golint ./internal/...
 
 for d in $(go list ./pkg/... | grep -v vendor); do
     go test -race -coverprofile=profile.out -covermode=atomic $d
@@ -24,4 +22,6 @@ for d in $(go list ./pkg/... | grep -v vendor); do
         rm profile.out
     fi
 done
+
+curl -s https://codecov.io/bash | bash -s - -t 4ffaaabc-09b8-4ca5-9c22-6eff395fc643
 
